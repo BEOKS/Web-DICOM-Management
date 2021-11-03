bde978f00b75b4d0503f59f93b9342c118e2616a
LEEJAESEONG <lee01042000@gmail.com>
2021-11-03 14:10:51 +0900

    Feat(DicomServer/setting.py): add package and MiddleWare

--- DicomServer/settings.py
+++ DicomServer/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'DicomServiceApp.apps.DicomserviceappConfig'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True # when production, it must be revised
+
 MIDDLEWARE = [
+    'corsheaders.middlware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 - [x] 1. set Docker-compose
   - [x] 1-1. init Django - init complete
   - [x] 1-2. init MongoDB - init complete
+  - [ ] 1-3. connect Django and MongoDB
 - [ ] 2. Upload Dicom file info to mongoDB
 - [ ] 3. Make RESTful api
   - [ ] 3-1. GET: Downlaod mongoDB info
