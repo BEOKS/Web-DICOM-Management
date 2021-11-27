c6aafdfd90b5538c92e4d9a043d86ab006680361
BEOKS <lee01042000@gmail.com>
2021-11-27 23:05:08 +0900

    Fix(server.sh): add spring build command

--- server/server.sh
+++ server/server.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 cd DSMP
+./gradlew build -x test
+java -jar build/libs/gs-spring-boot-docker-0.1.0.jar
 mkdir -p build/dependency
 cd build/dependency 
 echo 'Start update SpringBoot project jar file...'
 jar -xf ../libs/DSMP-0.0.1-SNAPSHOT.jar
 echo 'DSMP-SpringBoot Project Update Complete'
-docker-compose $1
+docker-compose $1 
