46a7555f046a3d9e5dccb3ff9398a1837b6ad3f2
BEOKS <lee01042000@gmail.com>
2021-11-28 13:23:02 +0900

    Feat(Docker): spring build env is moved to docker now.

--- server/server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd DSMP
-./gradlew build -x test
-mkdir -p build/dependency
-cd build/dependency 
-echo 'Start update SpringBoot project jar file...'
-jar -xf ../libs/DSMP-0.0.1-SNAPSHOT.jar
-echo 'DSMP-SpringBoot Project Update Complete'
-docker-compose $1 
