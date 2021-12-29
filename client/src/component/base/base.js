a0182f45a7e37317cd12360832d13ecfcfd5fbc9
dohee121 <dohee121@naver.com>
2021-12-29 18:31:39 +0900

    Feat : Move drawer close button to right
    Epic: #27
    Task: #29

--- client/src/component/base/base.js
+++ client/src/component/base/base.js
@@ -44,7 +44,7 @@ const closedMixin = (theme) => ({
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-end',
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
