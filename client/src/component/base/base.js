b8ad675255ca4ea012b617adb7a8a6bda65370be
dohee121 <dohee121@naver.com>
2021-12-29 18:18:00 +0900

    Feat : Update the base frame
    Epic: #27
    Task: #29

--- client/src/component/base/base.js
+++ client/src/component/base/base.js
@@ -15,7 +15,8 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import { Inbox, Mail } from '@mui/icons-material';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import MoreIcon from '@mui/icons-material/More';
 
 const drawerWidth = 240;
 
@@ -43,7 +44,7 @@ const closedMixin = (theme) => ({
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
@@ -114,7 +115,7 @@ export default function MiniDrawer() {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" noWrap component="div">
-                        Mini variant drawer
+                        Dicom List
                     </Typography>
                 </Toolbar>
             </AppBar>
@@ -126,10 +127,10 @@ export default function MiniDrawer() {
                 </DrawerHeader>
                 <Divider />
                 <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    {['Project 1', 'Project 2', 'Project 3', 'Project 4'].map((text) => (
                         <ListItem button key={text}>
                             <ListItemIcon>
-                                {index % 2 === 0 ? <Inbox /> : <Mail />}
+                                <MedicalServicesIcon />
                             </ListItemIcon>
                             <ListItemText primary={text} />
                         </ListItem>
@@ -137,10 +138,10 @@ export default function MiniDrawer() {
                 </List>
                 <Divider />
                 <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    {['ETC'].map((text) => (
                         <ListItem button key={text}>
                             <ListItemIcon>
-                                {index % 2 === 0 ? <Inbox /> : <Mail />}
+                                <MoreIcon />
                             </ListItemIcon>
                             <ListItemText primary={text} />
                         </ListItem>
@@ -149,33 +150,33 @@ export default function MiniDrawer() {
             </Drawer>
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 <DrawerHeader />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                    sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                    posuere sollicitudin aliquam ultrices sagittis orci a.
-                </Typography>
+                {/*<Typography paragraph>*/}
+                {/*    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod*/}
+                {/*    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non*/}
+                {/*    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus*/}
+                {/*    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.*/}
+                {/*    Convallis convallis tellus id interdum velit laoreet id donec ultrices.*/}
+                {/*    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit*/}
+                {/*    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra*/}
+                {/*    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum*/}
+                {/*    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis*/}
+                {/*    feugiat vivamus at augue. At augue eget arcu dictum varius duis at*/}
+                {/*    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa*/}
+                {/*    sapien faucibus et molestie ac.*/}
+                {/*</Typography>*/}
+                {/*<Typography paragraph>*/}
+                {/*    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper*/}
+                {/*    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim*/}
+                {/*    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra*/}
+                {/*    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis*/}
+                {/*    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi*/}
+                {/*    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit*/}
+                {/*    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem*/}
+                {/*    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis*/}
+                {/*    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis*/}
+                {/*    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla*/}
+                {/*    posuere sollicitudin aliquam ultrices sagittis orci a.*/}
+                {/*</Typography>*/}
             </Box>
         </Box>
     );
