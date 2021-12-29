faf342ba2f74c46ae02e65f0196bf3aa7e085b03
dohee121 <dohee121@naver.com>
2021-12-29 20:08:49 +0900

    Refactor : Change camelCase to PascalCase
    client/src/component 내부 파일 이름을 파스칼 표기법으로 통일
    
    Epic: #27
    Task: #31

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Page from './component/base/base';
+import Page from './component/Base/Base';
 
 export default class Main extends React.Component {
     render() {
