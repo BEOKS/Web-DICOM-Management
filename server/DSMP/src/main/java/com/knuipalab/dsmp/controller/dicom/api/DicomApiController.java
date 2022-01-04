c94855054ca9ab88151ae66358267df3e44b5f1c
BongYang <33821003+BongYang@users.noreply.github.com>
2022-01-04 16:42:08 +0900

    ADD: completed Upload & Download API

--- server/DSMP/src/main/java/com/knuipalab/dsmp/controller/dicom/api/DicomApiController.java
+++ server/DSMP/src/main/java/com/knuipalab/dsmp/controller/dicom/api/DicomApiController.java
@@ -2,24 +2,11 @@ package com.knuipalab.dsmp.controller.dicom.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import okhttp3.*;
-import org.json.JSONObject;
 import org.springframework.http.*;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 
 @RestController
@@ -35,21 +22,19 @@ public class DicomApiController {
         System.out.println("File Size: " + file.getSize());
         System.out.println("File: " + file.toString());
 
-        String BASE_URL = "http://localhost:8042/instances";
-//        String BASE_URL = "https://demo.orthanc-server.com/instances";
+        String request_URL = "http://orthanc:8042/instances";
 
         RestTemplate restTemplate = new RestTemplate();
 
-//        header setting
+        // header setting
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/dicom");
 
-//        post api call
+        // post api call
         byte[] content = file.getBytes();
         HttpEntity<byte []> requestEntity = new HttpEntity<>(content, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(BASE_URL, requestEntity, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(request_URL, requestEntity, String.class);
 
-//        reponse to json
         System.out.println("-----------Response Result------------");
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode;
@@ -67,64 +52,21 @@ public class DicomApiController {
 
     @GetMapping("/api/dicom")
     public String listDicom(){
-        String BASE_URL = "http://localhost:8042/instances";
+        // String request_URL = "http://localhost:8042/instances";
+        String request_URL = "http://orthanc:8042/instances";
+
         RestTemplate restTemplate = new RestTemplate();
 
-//        get api call
-        ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL, String.class);
+        // get api call
+        ResponseEntity<String> response = restTemplate.getForEntity(request_URL, String.class);
         System.out.println(response.toString());
 
         return response.toString();
     }
 
-    @PostMapping("/api/dicom3")
-    public JSONObject UserLogoutInterface(@RequestPart("dicomfile") MultipartFile file) throws IOException {
-        String BASE_URL = "http://localhost:8042/instances";
-
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-
-        File newfile = new File(file.getOriginalFilename());
-        file.transferTo(newfile);
-
-        System.out.println(newfile.getName());
-        System.out.println(newfile.getPath());
-
-        // 전달하고자 하는 데이터를 JSON 형식으로 FormDataPart에 삽입.
-        RequestBody body = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("file", file.getName(),
-                        RequestBody.create(okhttp3.MediaType.parse("application/dicom"), newfile))
-                .build();
-
-        // url안의 주소는 호출하고자 하는 API의 주소 및 포트번호
-        Request request = new Request.Builder()
-                .header("Content-Type", "application/dicom")
-                .url(BASE_URL)
-                .post(body)
-                .build();
-
-        System.out.println(request.toString());
-//        System.out.println(request.body());
-
-        Response response = client.newCall(request).execute();
-//        System.out.println(response.toString());
-        System.out.println(response.headers().toString());
-//        System.out.println(response.body().toString());
-//        if (response.isSuccessful()) {
-//            ResponseBody rbody = response.body();
-//
-//            if (rbody != null)
-//                System.out.println("rbody" + rbody.toString());
-//                System.out.println("jsonText" + jsonText.toString());
-//                rbody.close();
-//
-//                JSONObject json = new JSONObject(jsonText);
-//            }
-//        }
-//        System.out.println("aaa" + response.body().string().toString());
-
-//        JSONObject json = new JSONObject(response.body().string());
-        JSONObject json = new JSONObject("{}");
-        return json;
+    @PostMapping("/api")
+    public String test(@RequestParam MultipartFile file){
+        System.out.println(file.getSize());
+        return "Hello ";
     }
 }
