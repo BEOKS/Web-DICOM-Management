c0afe4657f9483d83c2984eeedddec257045a5fd
dohee121 <dohee121@naver.com>
2022-01-04 17:39:34 +0900

    Refactor : Seperate drawer, app bar from App.js
    - App.js에서 드로어와 상단바를 분리하여 새 파일을 생성합니다.
    - src/componenet/AppBar/BaseAppBar.js
    - src/omponent/Drawer/ProjectDrawer.js
    
    Epic: #27
    Task: #63

---
+++ client/src/component/Drawer/ProjectDrawer.js
@@ -0,0 +1,98 @@
+import { styled } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import MoreIcon from '@mui/icons-material/More';
+import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
+
+export const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(9)} + 1px)`,
+    },
+});
+
+export const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+export const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    })
+);
+
+export default function ProjectDrawer(props) {
+    const theme = useTheme();
+
+    return (
+        <Drawer variant="permanent" open={props.open}>
+            <DrawerHeader>
+                <IconButton onClick={props.handleDrawerClose}>
+                    {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                </IconButton>
+            </DrawerHeader>
+            <Divider />
+            <List>
+                {['Project 1', 'Project 2', 'Project 3', 'Project 4'].map((text) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>
+                            <MedicalServicesIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['ETC'].map((text) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>
+                            <MoreIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    );
+}
