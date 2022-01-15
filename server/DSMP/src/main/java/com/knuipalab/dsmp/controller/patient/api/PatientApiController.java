7f75f39f5f1bb94f708624c40a6598333aab1b25
leeseungmin4966 <bae8416o@naver.com>
2022-01-15 18:05:19 +0900

    Feat : Patient Collection 설계 완료
    1. metadata 업로드시 metadata 내 anonymized_id를 참조해 patient의 referencedCount 를 올려준다. (기존의 환자가 있다면 referencedCount만 올려주고, 없다면 anonymized_id로 환자 생성후 referencedCount를 올려준다.)
    
    2. metadata 삭제 시, metadata body에서 anonymized_id가 참조하는 환자의 referencedCount를 감소시켜 준다.

---
+++ server/DSMP/src/main/java/com/knuipalab/dsmp/controller/patient/api/PatientApiController.java
@@ -0,0 +1,22 @@
+package com.knuipalab.dsmp.controller.patient.api;
+
+
+import com.knuipalab.dsmp.service.patient.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PatientApiController {
+
+    @Autowired
+    PatientService patientService;
+
+    @GetMapping("api/Patient/nonReferenced")
+    public void findNonReferencedPatients(){
+        patientService.findNonReferencedPatients();
+    }
+
+}
