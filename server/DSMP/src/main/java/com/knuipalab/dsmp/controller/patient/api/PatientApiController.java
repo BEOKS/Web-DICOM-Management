8b68d353d894b668dbd17e7c51766a616e628594
leeseungmin4966 <bae8416o@naver.com>
2022-01-18 15:38:26 +0900

    Feat : Login 기능 테스트를 위한 mustache 수정
    벡엔드에서 Login 기능 테스트를 위해 home 페이지에 로그인성공시 유저의 이름을 get해 올수 있도록 수정하였다.

--- server/DSMP/src/main/java/com/knuipalab/dsmp/controller/patient/api/PatientApiController.java
+++ server/DSMP/src/main/java/com/knuipalab/dsmp/controller/patient/api/PatientApiController.java
@@ -1,11 +1,14 @@
 package com.knuipalab.dsmp.controller.patient.api;
 
 
+import com.knuipalab.dsmp.configuration.auth.dto.SessionUser;
+import com.knuipalab.dsmp.domain.user.User;
 import com.knuipalab.dsmp.service.patient.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
