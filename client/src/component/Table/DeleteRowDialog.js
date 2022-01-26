e5a988b5650f1dacb6489339746f7b4e5944d174
dohee121 <dohee121@naver.com>
2022-01-26 23:57:10 +0900

    Feat : Edit content of delete row dialog
    테이블 행 삭제 시 다이얼로그에 있던 두 가지 옵션을 없애고
    메타 데이터와 Dicom을 동시에 삭제하는 것만 가능하도록 변경합니다.
    삭제 다이얼로그에서는 옵션 선택이 아닌 확인 기능만을 수행합니다.
    
    Task: #130

--- client/src/component/Table/DeleteRowDialog.js
+++ client/src/component/Table/DeleteRowDialog.js
@@ -4,16 +4,9 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Alert from '@mui/material/Alert';
 import axios from 'axios';
 
 export default function DeleteRowDialog(props) {
-    const [value, setValue] = React.useState('');
     const { open, onClose, selected, selectedPatientIDList, metaDataUpdated, setMetaDataUpdated } = props;
 
     const deleteMetaData = () => {
@@ -34,17 +27,9 @@ export default function DeleteRowDialog(props) {
         })
     };
 
-    const handleRadioChange = (event) => {
-        setValue(event.target.value);
-    };
-
     const handleSubmit = (event) => {
-        // event.preventDefault();
         deleteMetaData();
-
-        if (value === 'metaDataAndDicom') {
-            deleteDicom();
-        }
+        deleteDicom();
         setMetaDataUpdated(!metaDataUpdated);
         onClose();
     };
@@ -58,24 +43,10 @@ export default function DeleteRowDialog(props) {
                 aria-describedby="alert-dialog-description"
             >
                 <DialogTitle id="alert-dialog-title">
-                    {"선택한 행 삭제"}
+                    {"Dicom 삭제"}
                 </DialogTitle>
                 <DialogContent>
-                    <Alert severity="info">메타 데이터만 삭제할 경우, 해당 메타 데이터와 매핑되었던 Dicom 파일은
-                        <br />Non-Reference Dicom 카테고리에서 확인할 수 있습니다.</Alert>
-                    <FormControl component="fieldset" sx={{ mt: 3 }}>
-                        <FormLabel component="legend">삭제 옵션을 선택해주세요.</FormLabel>
-                        <RadioGroup
-                            aria-label="gender"
-                            defaultValue="onlyMetaData"
-                            name="radio-buttons-group"
-                            value={value}
-                            onChange={handleRadioChange}
-                        >
-                            <FormControlLabel value="onlyMetaData" control={<Radio />} label="메타 데이터 삭제" />
-                            <FormControlLabel value="metaDataAndDicom" control={<Radio />} label="메타 데이터와 Dicom 파일 모두 삭제" />
-                        </RadioGroup>
-                    </FormControl>
+                    선택한 행의 메타 데이터 및 Dicom 파일을 삭제합니다.
                 </DialogContent>
                 <DialogActions>
                     <Button
