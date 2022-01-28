f81494be382c07ccfaf9c7f262629f7709549a65
BEOKS <lee01042000@gmail.com>
2022-01-28 16:15:04 +0900

    Feat : check login status
    이제 페이지가 시작할 때,
    로그인을 검사 기능이 수행됩니다.
    
    Task : #136

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import * as React from 'react'
 import {useState} from 'react';
 import Page from './Page'
 import LoadingPage from './component/Login/loading';
+import { chcekLoginStatusAsync } from './component/Login/Login';
 export default function App(){ 
-    const [loginStatus,setLoginStatus]=useState()
-
+    const [loginStatus,setLoginStatus]=useState();
     if(loginStatus===undefined){
+        chcekLoginStatusAsync(setLoginStatus);
         return(
             <LoadingPage message="사용자 정보를 확인중입니다."/>
         )
+    }
+    else if(loginStatus===false){
+        window.location.href='/oauth2/authorization/google'
     }
     else{
         return(<Page/>)
