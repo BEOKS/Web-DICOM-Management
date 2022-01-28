aaf3634d420994e3fe5e492c2db441b825357389
dohee121 <dohee121@naver.com>
2022-01-28 17:47:06 +0900

    Fix : Solve 'Failed prop type' error
    LoadingPage의 Dialog 컴포넌트에서 open prop에
    boolean type이 아닌 string type을 전달하여
    에러가 발생하였으므로 이를 수정합니다.
    
    Task: #130

--- client/src/component/Login/loading.js
+++ client/src/component/Login/loading.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { CircularProgress, Dialog, DialogContent, Stack, Typography} from '@mui/material'
 export default function LoadingPage({message}){
     return(
-        <Dialog open='true'>
+        <Dialog open={true}>
             <DialogContent>
                 <Stack alignItems="center">
                     <CircularProgress margin={2}/>
