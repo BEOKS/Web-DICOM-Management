82fadd1b226f7f35cfb479b6b1765fdd72d84dcd
BEOKS <lee01042000@gmail.com>
2022-01-28 15:24:12 +0900

    Feat : add loading page
    메인 페이지에 진입하기 전, 로그인 여부를 확인할 때
    사용자에게 보여줄 로딩 페이지를 추가했습니다.
    
    Task : #136

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const hostLocation=process.env.SERVER_HOST
+const hostLocation=process.env.REACT_APP_SERVER_HOST
 module.exports = function (app) {
     app.use(
         createProxyMiddleware('/MetaData', {
