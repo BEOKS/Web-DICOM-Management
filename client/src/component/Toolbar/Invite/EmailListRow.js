7f8e1993d347b23d850b8e728d249d61d7449565
dohee121 <dohee121@naver.com>
2022-02-18 00:07:15 +0900

    Feat : Validate email address, check already exist
    1. 초대 다이얼로그의 전체적인 뷰를 수정합니다.
       이메일로 초대하는 부분과 이메일 리스트를 소제목으로 구분합니다.
       이메일 리스트 행의 radius 및 divider를 추가합니다.
    
    2. 정규표현식을 이용하여 이메일 유효성을 검사합니다.
    
    3. 이미 초대한 계정인지 확인합니다.
    
    Task: #177

--- client/src/component/Toolbar/Invite/EmailListRow.js
+++ client/src/component/Toolbar/Invite/EmailListRow.js
@@ -1,27 +1,45 @@
 import * as React from 'react';
-import { Alert, IconButton } from '@mui/material';
+import { Alert, Divider, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 export default function EmailListRow(props) {
-    const { email, emailArray, setEmailArray } = props;
+    const { email, index, emailArray, setEmailArray } = props;
+
+    const getRadius = () => {
+        if (index === 0 && index === emailArray.length - 1) {
+            return '4px 4px 4px 4px';
+        } else if (index === 0 && index !== emailArray.length - 1) {
+            return '4px 4px 0px 0px';
+        } else if (index === emailArray.length - 1) {
+            return '0px 0px 4px 4px';
+        } else {
+            return '0px 0px 0px 0px';
+        }
+    };
 
     return (
-        <Alert severity="success"
-            action={
-                <IconButton
-                    aria-label="close"
-                    color="inherit"
-                    size="small"
-                    onClick={() => {
-                        setEmailArray(emailArray.filter(i => i !== email));
-                    }}
-                >
-                    <CloseIcon fontSize="inherit" />
-                </IconButton>
-            }
-        >
-            {email}
-
-        </Alert>
+        <React.Fragment>
+            <Alert 
+                severity="info"
+                icon={<MailOutlineIcon fontSize="inherit" />}
+                action={
+                    <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"
+                        onClick={() => {
+                            setEmailArray(emailArray.filter(i => i !== email));
+                        }}
+                    >
+                        <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                }
+                sx={{ borderRadius: getRadius() }}
+            >
+                {email}
+            </Alert>
+            {index !== emailArray.length - 1 && <Divider color="#d9f3ff"/>}
+        </React.Fragment>
     );
 }
--- client/src/component/Toolbar/Invite/InviteDialog.js
+++ client/src/component/Toolbar/Invite/InviteDialog.js
@@ -1,53 +1,85 @@
 import * as React from 'react';
-import { Dialog, DialogContent, DialogActions, Button, Grid, TextField, Stack } from '@mui/material';
+import { Dialog, DialogContent, DialogActions, Button, Grid, TextField, Stack, Typography } from '@mui/material';
 import { useState } from 'react';
 import EmailListRow from './EmailListRow';
+import './InviteDialog.css';
 
 export default function InviteDialog(props) {
     const { open, setOpen } = props;
     const [email, setEmail] = useState('');
     const [emailArray, setEmailArray] = useState([]);
+    const [isEmail, setIsEmail] = useState(true);
+    const [alreadyExist, setAlreadyExist] = useState(false);
+
+    const checkEmail = (email) => {
+        const emailRegex =
+            /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+        const result = emailRegex.test(email);
+        setIsEmail(result);
+        return result;
+    };
+
+    const checkAlreadyExist = (email) => {
+        const exist = emailArray.some(i => i === email);
+        setAlreadyExist(exist);
+        return exist;
+    };
 
     const handleInviteClick = () => {
-        const newEmailArray = emailArray.concat(email);
-        setEmailArray(newEmailArray);
-        setEmail('');
+        if (checkEmail(email) && !checkAlreadyExist(email)) {
+            const newEmailArray = emailArray.concat(email);
+            setEmailArray(newEmailArray);
+            setEmail('');
+        }
     };
 
     const handleOKClick = () => {
         setEmailArray([]);
         setEmail('');
+        setIsEmail(true);
+        setAlreadyExist(false);
         setOpen(false);
     };
 
     const handleCancelClick = () => {
         setEmailArray([]);
         setEmail('');
+        setIsEmail(true);
+        setAlreadyExist(false);
         setOpen(false);
     };
 
     return (
         <Dialog open={open}>
             <DialogContent>
-                <Grid container columnSpacing={1} sx={{ pt: 1 }}>
+                <Typography variant="subtitle2" gutterBottom component="div" color="#014361">
+                    <span className="divider" />이메일로 초대하기
+                </Typography>
+                <Grid container columnSpacing={1}>
                     <Grid item xs>
                         <TextField
                             autoFocus
                             label="Email"
-                            helperText="이메일로 초대하기"
                             size="small"
                             onChange={e => { setEmail(e.target.value) }}
                             value={email}
+                            error={(!isEmail && true) || (alreadyExist && true)}
+                            helperText={(!isEmail && "이메일 형식이 아닙니다.") || (alreadyExist && "이미 초대한 계정입니다.")}
                         />
                     </Grid>
                     <Grid item xs='auto'>
                         <Button onClick={handleInviteClick} variant="contained" sx={{ py: '7.75px' }}>초대</Button>
                     </Grid>
                 </Grid>
-                <Stack sx={{mt: 1}}>
-                    {emailArray.map((email) => {
+                {emailArray.length > 0 && (
+                    <Typography variant="subtitle2" gutterBottom component="div" color="#014361" sx={{ mt: 3 }}>
+                        <span className="divider" />프로젝트에 초대된 계정
+                    </Typography>
+                )}
+                <Stack>
+                    {emailArray.map((email, index) => {
                         return (
-                            <EmailListRow key={email} email={email} emailArray={emailArray} setEmailArray={setEmailArray}></EmailListRow>
+                            <EmailListRow key={email} email={email} index={index} emailArray={emailArray} setEmailArray={setEmailArray}></EmailListRow>
                         );
                     })}
                 </Stack>
