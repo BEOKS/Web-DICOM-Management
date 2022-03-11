c79ad06e8d6841b8b9c85cf7d73647261cbee233
JAESEONG LEE <lee01042000@gmail.com>
2022-03-12 00:14:03 +0900

    Feat : show participant information of project(#211)
    이제 프로젝트에 참여하고 있는 Creator와 Visitor의 정보를
    프로젝트 테이블 상단에서 볼 수 있습니다.

---
+++ client/src/component/Toolbar/ProjectParticipant/ParticipantInfo.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import {Alert, Avatar, CircularProgress, Stack, Tooltip, Typography} from "@mui/material";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../../store";
+import {LOADING_STATUS, ParticipantInfoAction, Participants, User} from "./ParticipantInfoReducer";
+import axios from "axios";
+
+const getParticipantInfo=(responseCallback : (participant : Participants)=>void, errorCallback : ()=>void)=>{
+    axios.get('/api/Project/invited')
+        .then(response=>{
+            responseCallback(response.data.body[0])
+        })
+        .catch(error=>{
+            errorCallback()
+        })
+}
+export default function ParticipantInfo(){
+    const loadingStatus : LOADING_STATUS= useSelector((state : RootState)=>state.ParticipantInfoReducer.loadingStatus)
+    const participantInfo : Participants=useSelector((state:RootState)=> state.ParticipantInfoReducer.participants)
+    const dispatch = useDispatch()
+    if(loadingStatus===LOADING_STATUS.LOADING){
+        getParticipantInfo(
+            (participantInfo : Participants)=>{
+                dispatch(ParticipantInfoAction.setParticipant(participantInfo))
+            },()=>{dispatch(ParticipantInfoAction.setErrorState())})
+    }
+    return(
+        <Stack >
+            {loadingStatus===LOADING_STATUS.LOADING &&
+                <Stack direction={"row"} style={{ verticalAlign: "middle" }}>
+                    <Typography style={{ verticalAlign: "middle" }}>Loading User Information...</Typography>
+                    <CircularProgress style={{padding: "7px"}}/>
+                </Stack>
+            }
+            {loadingStatus===LOADING_STATUS.ERROR &&
+                <Alert  severity={'error'}>참여자 정보를 가져오는데 실패했습니다.</Alert>
+            }
+            {loadingStatus===LOADING_STATUS.FINISH &&
+                <Stack direction={"row"}>
+                    <Tooltip title={`Creator, ${participantInfo.creator.name}`} key={0}>
+                        <Avatar alt={participantInfo.creator.name} src={participantInfo.creator.picture}/>
+                    </Tooltip>
+                    {
+                        participantInfo.visitor.map((visitor :User,index)=>
+                            <Tooltip title={`${participantInfo.creator.name}`} key={index+1}>
+                                <Avatar alt={visitor.name} src={visitor.picture}/>
+                            </Tooltip>)
+                    }
+                </Stack>
+            }
+        </Stack>
+    )
+}
---
+++ client/src/component/Toolbar/ProjectParticipant/ParticipantInfoReducer.ts
@@ -0,0 +1,72 @@
+export type User={
+    userId : string,
+    name : string,
+    email : string,
+    picture : string, // picture link
+    role : string
+}
+export type Participants={
+    projectId : string,
+    projectName : string,
+    creator : User,
+    visitor : User[]
+}
+
+export enum LOADING_STATUS{
+    LOADING,
+    ERROR,
+    FINISH,
+    WAIT
+}
+
+type ParticipantInfoState={
+    participants : Participants
+    loadingStatus : LOADING_STATUS
+}
+
+const INIT_PARTICIPANT_INFO_STATE: ParticipantInfoState={
+    participants : {
+        projectId : '',
+        projectName : '',
+        creator : {userId : '',name : '',email : '',picture : '',role : ''},
+        visitor : []
+    },
+    loadingStatus: LOADING_STATUS.LOADING,
+
+}
+
+const HEADER='ParticipantInfoReducer'
+export const TYPE={
+    SET_PARTICIPANT : `${HEADER}/SET_PARTICIPANT` as const,
+    SET_LOADING_COMPLETE : `${HEADER}/SET_LOADING_COMPLETE` as const,
+    SET_LOADING_START : `${HEADER}/SET_LOADING_START` as const,
+    SET_ERROR_STATE : `${HEADER}/SET_ERROR_STATE` as const
+}
+
+export const ParticipantInfoAction={
+    setParticipant : (participant : Participants)=>({type: TYPE.SET_PARTICIPANT,payload : participant}),
+    setLoadingComplete : ()=>({type: TYPE.SET_LOADING_COMPLETE}),
+    setLoadingStart : ()=>({type: TYPE.SET_LOADING_START}),
+    setErrorState : ()=>({type : TYPE.SET_ERROR_STATE})
+}
+
+type ParticipantInfoActionType=
+    ReturnType<typeof ParticipantInfoAction.setParticipant> |
+    ReturnType<typeof ParticipantInfoAction.setLoadingComplete> |
+    ReturnType<typeof ParticipantInfoAction.setLoadingStart> |
+    ReturnType<typeof ParticipantInfoAction.setErrorState>
+
+export default function ParticipantInfoReducer(state : ParticipantInfoState=INIT_PARTICIPANT_INFO_STATE,action : ParticipantInfoActionType): ParticipantInfoState{
+    switch (action.type){
+        case TYPE.SET_PARTICIPANT:
+            return {...state,participants : action.payload,loadingStatus: LOADING_STATUS.FINISH}
+        case TYPE.SET_LOADING_COMPLETE:
+            return {...state,loadingStatus : LOADING_STATUS.FINISH}
+        case TYPE.SET_LOADING_START:
+            return {...state,loadingStatus : LOADING_STATUS.LOADING}
+        case TYPE.SET_ERROR_STATE:
+            return {...state,loadingStatus :LOADING_STATUS.ERROR}
+        default :
+            return state;
+    }
+}
