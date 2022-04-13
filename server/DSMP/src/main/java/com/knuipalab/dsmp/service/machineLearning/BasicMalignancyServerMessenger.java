acdf1e2afeea02b9f4956defd396895610ce9319
JAESEONG LEE <lee01042000@gmail.com>
2022-04-13 21:21:05 +0900

    Feat  : http request and parse to json(#236)

--- server/DSMP/src/main/java/com/knuipalab/dsmp/service/machineLearning/BasicMalignancyServerMessenger.java
+++ server/DSMP/src/main/java/com/knuipalab/dsmp/service/machineLearning/BasicMalignancyServerMessenger.java
@@ -2,13 +2,19 @@ package com.knuipalab.dsmp.service.machineLearning;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.knuipalab.dsmp.service.storage.FileSystemStorageService;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.net.http.HttpClient;
-import java.net.http.HttpResponse;
+import java.io.IOException;
 
 @Primary
 @Service
@@ -21,10 +27,32 @@ public class BasicMalignancyServerMessenger implements MalignancyServerMessenger
         this.fileSystemStorageService=fileSystemStorageService;
     }
 
+    private HttpResponse postImageToServer(String url,File file) {
+        HttpEntity entity= MultipartEntityBuilder.create()
+                .addPart("data",new FileBody(file))
+                .build();
+        HttpPost request=new HttpPost(url);
+        request.setEntity(entity);
+        HttpClient client = HttpClientBuilder.create().build();
+        try {
+            return client.execute(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     @Override
     public JsonNode requestMalignancyInference(String projectId, String imageName) {
         File imageFile=this.fileSystemStorageService.serveFile(projectId,imageName);
+        HttpResponse httpResponse=postImageToServer(URL,imageFile);
+        try {
+            String json = EntityUtils.toString(httpResponse.getEntity());
+            System.out.println("asdf"+json);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
         return null;
     }
 
