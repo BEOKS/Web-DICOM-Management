74c5be14d1944c35629690144f59e6dbb0eea322
doheez <doheedev@gmail.com>
2022-05-07 14:28:23 +0900

    Feat : choose ML model by select component
    기존엔 툴바의 ML 버튼 클릭 시
    머신러닝 추론을 진행할 것인지를 묻는 다이얼로그를 띄워
    데이터에 단일 모델만을 적용했다.
    
    이제 다이얼로그에 Select 컴포넌트를 추가해서
    모델을 사용자가 직접 선택할 수 있도록 하고
    확인 버튼 클릭 시 해당 모델을 적용한 머신러닝 추론을 진행한다.
    
    getModelList와 inference는 임시로 dummy API를 사용한다.
    
    Task: #252

--- client/src/component/Toolbar/MachineLearning/MLReducer.ts
+++ client/src/component/Toolbar/MachineLearning/MLReducer.ts
@@ -5,7 +5,9 @@ const TYPE = {
     CLOSE_DIALOG: `${HEADER}/CLOSE_DIALOG` as const,
     OPEN_SNACKBAR: `${HEADER}/OPEN_SNACKBAR` as const,
     UPDATE_SNACKBAR: `${HEADER}/UPDATE_SNACKBAR` as const,
-    CLOSE_SNACKBAR: `${HEADER}/CLOSE_SNACKBAR` as const
+    CLOSE_SNACKBAR: `${HEADER}/CLOSE_SNACKBAR` as const,
+    SET_MODEL_LIST: `${HEADER}/SET_MODEL_LIST` as const,
+    SET_SELECTED_MODEL: `${HEADER}/SET_SELECTED_MODEL` as const,
 };
 
 export const MLAction = {
@@ -13,7 +15,9 @@ export const MLAction = {
     closeDialog: () => ({ type: TYPE.CLOSE_DIALOG }),
     openSnackbar: () => ({ type: TYPE.OPEN_SNACKBAR }),
     updateSnackbar: () => ({ type: TYPE.UPDATE_SNACKBAR }),
-    closeSnackbar: () => ({ type: TYPE.CLOSE_SNACKBAR })
+    closeSnackbar: () => ({ type: TYPE.CLOSE_SNACKBAR }),
+    setModelList: (modelList: string[]) => ({ type: TYPE.SET_MODEL_LIST, payload: modelList}),
+    setSelectedModel: (model: string) => ({type: TYPE.SET_SELECTED_MODEL, payload: model})
 };
 
 type MLActionType =
@@ -21,7 +25,9 @@ type MLActionType =
     ReturnType<typeof MLAction.closeDialog> |
     ReturnType<typeof MLAction.openSnackbar> |
     ReturnType<typeof MLAction.updateSnackbar> |
-    ReturnType<typeof MLAction.closeSnackbar>;
+    ReturnType<typeof MLAction.closeSnackbar> |
+    ReturnType<typeof MLAction.setModelList> |
+    ReturnType<typeof MLAction.setSelectedModel>;
 
 // state
 type snackbarInfoType = {
@@ -33,6 +39,8 @@ type snackbarInfoType = {
 type MLStateType = {
     dialogOpen: boolean,
     snackbarInfo: snackbarInfoType
+    modelList: string[],
+    selectedModel: string
 };
 
 const INIT_SAMPLING_STATE: MLStateType = {
@@ -41,7 +49,9 @@ const INIT_SAMPLING_STATE: MLStateType = {
         open: false,
         message: 'Machine Learning...',
         progress: 0
-    }
+    },
+    modelList: [],
+    selectedModel: ''
 };
 
 // reducer
@@ -50,13 +60,17 @@ export default function SamplingReducer(state: MLStateType = INIT_SAMPLING_STATE
         case TYPE.OPEN_DIALOG:
             return { ...state, dialogOpen: true, snackbarInfo: { ...state.snackbarInfo, message: 'Machine Learning...', progress: 0 } };
         case TYPE.CLOSE_DIALOG:
-            return { ...state, dialogOpen: false };
+            return { ...state, dialogOpen: false, selectedModel: '' };
         case TYPE.OPEN_SNACKBAR:
             return { ...state, snackbarInfo: { ...state.snackbarInfo, open: true } };
         case TYPE.UPDATE_SNACKBAR:
             return { ...state, snackbarInfo: { ...state.snackbarInfo, message: 'Machine Learning Finished!', progress: 100 } };
         case TYPE.CLOSE_SNACKBAR:
             return { ...state, snackbarInfo: { ...state.snackbarInfo, open: false } };
+        case TYPE.SET_MODEL_LIST:
+            return { ...state, modelList: action.payload };
+        case TYPE.SET_SELECTED_MODEL:
+            return {...state, selectedModel: action.payload};
         default:
             return state;
     }
