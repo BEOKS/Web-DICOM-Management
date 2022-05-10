c89fa7e0ec853d04083b333b7ec68039e4f817d6
JAESEONG LEE <lee01042000@gmail.com>
2022-05-10 02:40:59 +0000

    Feat : 모델 리스트를 가져오는 API를 리액트에 연결

--- client/src/api/MachineLearning/Management.ts
+++ client/src/api/MachineLearning/Management.ts
@@ -1,6 +1,16 @@
 // dummy API
+import axios from "axios";
 export const getModelList = (callback: (modelList: string[]) => void) => {
-    callback(['model1', 'model2', 'model3', 'model4']);
+    axios.get('api/ML/getModelList')
+        .then(response=>{
+            const json = Array.from(response.data['models']).map((data : any) => data['modelName'])
+            callback(json)
+        })
+        .catch(error =>{
+            alert('사용 가능한 모델 검색 중 에러가 발생하였습니다. 다시 시도해주세요')
+            callback([])
+        })
+    // callback(['model1', 'model2', 'model3', 'model4']);
 };
 
 // dummy API
