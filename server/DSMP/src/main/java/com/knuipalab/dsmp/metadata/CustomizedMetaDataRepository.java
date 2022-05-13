2079f88d35c774bb55ea9ced10dff0eede6fc79a
LEESEUNGMIN <bae8416o@naver.com>
2022-05-14 02:27:10 +0900

    Feat : CustomizedMetaDataRepository 내 findByProjectIdWithPagingAndFiltering 쿼리를 추가
    issue : #272
    
    기능 추가 :
    
    CustomizedMetaDataRepository 내 findByProjectIdWithPagingAndFiltering API를 추가하여, 파라미터 값으로 받은 page, size, projectId 를 통해 해당 프로젝트 아이디를 가진 메타데이터 중 page 값에 해당 하는 메타데이터들을 size 만큼 받아 온다.
    
    MeataData QA로 실제 DB로 접근하여 테스팅시,
    
    * 기존 방식으로 10만개 기준 2sec 380ms가 소요되지만 size를 20으로 규정 후 paging 기법으로 찾으니, 330ms만에 해당 페이징 내에 있는 메타데이터 리스트를 찾아올 수 있었다.
    
    추가 방향 :
    
    1. 필터링 값들을 동적으로 적용할 수 있도록 수정한다.
    2. 개선율을 100만개 단위로 계산해 보고, 성능향상을 기록한다.

--- server/DSMP/src/main/java/com/knuipalab/dsmp/metadata/CustomizedMetaDataRepository.java
+++ server/DSMP/src/main/java/com/knuipalab/dsmp/metadata/CustomizedMetaDataRepository.java
@@ -5,4 +5,5 @@ import java.util.HashMap;
 public interface CustomizedMetaDataRepository {
     void updateType(String metadataId, String type);
     void setMalignancyClassification(String metadataId, HashMap classificationSet);
+    void findByProjectIdWithPagingAndFiltering(String projectId,int page,int size);
 }
