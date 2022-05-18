d7609593f5c155ffa5cc1b92d1843531637e749c
doheez <doheedev@gmail.com>
2022-05-18 20:15:00 +0900

    Feat : make the dicom download, row deletion work
    Data Grid 툴바의 Dicom 다운로드, 행 삭제 기능을 동작하도록 합니다.
    Dicom 다운로드 기능은 일시 중단 상태이므로 주석 처리해두었습니다.
    
    Task: #270

--- client/src/component/Table/MetaDataGrid.tsx
+++ client/src/component/Table/MetaDataGrid.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import './MetaDataGrid.css';
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from '../../store';
 import { MetaDataGridAction } from './MetaDataGridReducer';
@@ -9,7 +10,10 @@ import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import { extractBody, extractColumns } from "./Utils/extractMetaData";
 import exportCSVFile from "./Utils/exportCSVFile"
-import './MetaDataGrid.css';
+import axios from 'axios'
+import DeleteRowDialog from "./DeleteTestDialog";
+
+const hostLocation = process.env.SERVER_HOST ? process.env.SERVER_HOST : 'localhost'
 
 export type Body = {
     [key: string]: string
@@ -21,8 +25,8 @@ export type MetaData = {
     projectId: string
 };
 
-// Page 리팩토링 후 Page로 옮길 타입들
-type User = {
+// Page 리팩토링 후 Page로 옮길 타입
+export type User = {
     userId: string,
     name: string,
     email: string,
@@ -30,14 +34,15 @@ type User = {
     role: string
 };
 
-type Project = {
+// Page 리팩토링 후 Page로 옮길 타입
+export type Project = {
     creator: User,
     projectId: string,
     projectName: string,
     visitor: User[]
 };
 
-type MetaDataGridProps = {
+export type MetaDataGridProps = {
     metaData: MetaData[],
     project: Project
 };
@@ -48,12 +53,14 @@ const MetaDataGrid: React.FC<MetaDataGridProps> = ({ metaData, project }) => {
 
     const dispatch = useDispatch();
     const selectedRow = useSelector((state: RootState) => state.MetaDataGridReducer.selectedRow);
-    const selectedMetaDataID = useSelector((state: RootState) => state.MetaDataGridReducer.selectedMetaDataID);
 
+    // StudyUID 필드명이 바뀔 수 있으므로 따로 기재
+    const STUDY_UID_NAME = "StudyInstanceUID";
 
     const onSelectionChanged = ({ selectedRowsData }: { selectedRowsData: Body[] }) => {
         dispatch(MetaDataGridAction.setSelectedRow(selectedRowsData));
         dispatch(MetaDataGridAction.setSelectedMetaDataID(selectedRowsData.map((row: Body) => row.metadataId)));
+        dispatch(MetaDataGridAction.setSelectedStudyUID(selectedRowsData.map((row: Body) => row[STUDY_UID_NAME])));
     };
 
     const handleCSVDownloadButtonClick = () => {
@@ -68,6 +75,39 @@ const MetaDataGrid: React.FC<MetaDataGridProps> = ({ metaData, project }) => {
             exportCSVFile(selectedMetaData, project.projectName + '.csv');
         }
     };
+
+    const handleDicomDownloadButtonClick = () => {
+        //download files
+        if (selectedRow.length === 0) {
+            alert('다운로드할 Dicom을 선택해주세요.');
+        } else {
+            alert('현재 이미지 파일 시스템 개발을 위해 일시적으로 Dicom 서비스를 중단하였습니다. 추후 복구 예정입니다.')
+            // const selectedMetaData = selectedRow.map(row => {
+            //     delete row.metadataId;
+            //     return row;
+            // });
+            // selectedMetaData.forEach(metaData => {
+            //     const studyUID = metaData.StudyInstanceUID;
+            //     axios.get(`http://${hostLocation}:8080/api/study/${studyUID}/dicom`, { responseType: 'blob' })
+            //         .then((response) => {
+            //             const url = window.URL.createObjectURL(new Blob([response.data]));
+            //             const link = document.createElement('a');
+            //             link.href = url;
+            //             link.setAttribute('download', `${studyUID}.zip`); //or any other extension
+            //             document.body.appendChild(link);
+            //             link.click();
+            //         });
+            // });
+        }
+    };
+
+    const handleDeleteButtonClick = () => {
+        if (selectedRow.length > 0) {
+            dispatch(MetaDataGridAction.openDeleteRowDialog());
+        } else {
+            alert('삭제할 행을 선택해주세요.');
+        }
+    }
 
     return (
         <Box m={3} >
@@ -89,7 +129,10 @@ const MetaDataGrid: React.FC<MetaDataGridProps> = ({ metaData, project }) => {
                 {/* Toolbar를 다른 컴포넌트로 분리했더니 화면에 나타나지 않아서 그대로 둠 */}
                 <Toolbar>
                     <Item location="before">
-                        <Typography ml={1}>{selectedMetaDataID.length} selected</Typography>
+                        {selectedRow.length === 0
+                            ? <Typography variant="h6" ml={1}>Dicom List</Typography>
+                            : <Typography ml={1}>{selectedRow.length} selected</Typography>
+                        }
                     </Item>
                     <Item location="after">
                         <Tooltip title="Download CSV">
@@ -100,17 +143,18 @@ const MetaDataGrid: React.FC<MetaDataGridProps> = ({ metaData, project }) => {
                     </Item>
                     <Item location="after">
                         <Tooltip title="Download Dicom">
-                            <IconButton>
+                            <IconButton onClick={() => handleDicomDownloadButtonClick()}>
                                 <CloudDownloadIcon />
                             </IconButton>
                         </Tooltip>
                     </Item>
                     <Item location="after">
                         <Tooltip title="Delete">
-                            <IconButton>
+                            <IconButton onClick={() => handleDeleteButtonClick()}>
                                 <DeleteIcon />
                             </IconButton>
                         </Tooltip>
+                        <DeleteRowDialog project={project} />
                     </Item>
                 </Toolbar>
             </DataGrid>
