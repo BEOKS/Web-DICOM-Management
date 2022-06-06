ee4623cdb8ca8b2c335b40bc0268cb7327190ebd
JAESEONG LEE <lee01042000@gmail.com>
2022-06-06 07:40:20 +0000

    Fix : 페이징이 기존의 API를 침범하는 버그를 수정
    페이징 기능을 기존에 모든 데이터를 가져오는 API에
    사용하도록 하여 프론트에서는 잘못된 API를 호출하도록
    유도하는 버그를 수정하였다.

--- server/DSMP/src/main/java/com/knuipalab/dsmp/metadata/MetaDataApiController.java
+++ server/DSMP/src/main/java/com/knuipalab/dsmp/metadata/MetaDataApiController.java
@@ -22,49 +22,59 @@ public class MetaDataApiController {
     private MetaDataService metaDataService;
 
     // projectId가 같은 metadata를 모두 반환
-    @GetMapping("api/MetaData/{projectId}")
-    public ResponseEntity< ? extends BasicResponse> findByProjectIdWithPagingAndFiltering(@PathVariable String projectId, @RequestParam HashMap<String,Object> parmMap){
-        Page<MetaData> metaDataPage = metaDataService.findByProjectIdWithPagingAndFiltering(projectId,parmMap);
+    @GetMapping("api/MetaData/page/{projectId}")
+    public ResponseEntity<? extends BasicResponse> findByProjectIdWithPagingAndFiltering(@PathVariable String projectId,
+            @RequestParam HashMap<String, Object> parmMap) {
+        Page<MetaData> metaDataPage = metaDataService.findByProjectIdWithPagingAndFiltering(projectId, parmMap);
         return ResponseEntity.ok().body(new SuccessDataResponse<Page<MetaData>>(metaDataPage));
+    }
+
+    @GetMapping("api/MetaData/{projectId}")
+    public ResponseEntity<? extends BasicResponse> findByProjectId(@PathVariable String projectId,
+            MultipartFile multipartFile) {
+        List<MetaDataResponseDto> metaDataResponseDtos = metaDataService.findByProjectId(projectId);
+        return ResponseEntity.ok().body(new SuccessDataResponse<List<MetaDataResponseDto>>(metaDataResponseDtos));
     }
 
     // metatdata를 projectId와 함께 저장
     @PostMapping("api/MetaData/{projectId}")
-    public ResponseEntity<? extends  BasicResponse> insert(@PathVariable String projectId,@RequestBody Document body){
-        MetaDataCreateRequestDto metaDataCreateRequestDto = new MetaDataCreateRequestDto(projectId,body);
+    public ResponseEntity<? extends BasicResponse> insert(@PathVariable String projectId, @RequestBody Document body) {
+        MetaDataCreateRequestDto metaDataCreateRequestDto = new MetaDataCreateRequestDto(projectId, body);
         metaDataService.insert(metaDataCreateRequestDto);
         return ResponseEntity.ok().body(new SuccessResponse());
     }
 
     // 다수개의 metadata를 projectId와 함께 저장
     @PostMapping("api/MetaDataList/insert/{projectId}")
-    public ResponseEntity<? extends  BasicResponse> insertAllByMetaDataList(@PathVariable String projectId,@RequestBody List<Document> bodyList){
-        MetaDataCreateAllRequestDto metaDataCreateAllRequestDto = new MetaDataCreateAllRequestDto(projectId,bodyList);
+    public ResponseEntity<? extends BasicResponse> insertAllByMetaDataList(@PathVariable String projectId,
+            @RequestBody List<Document> bodyList) {
+        MetaDataCreateAllRequestDto metaDataCreateAllRequestDto = new MetaDataCreateAllRequestDto(projectId, bodyList);
         metaDataService.insertAllByMetaDataList(metaDataCreateAllRequestDto);
         return ResponseEntity.ok().body(new SuccessResponse());
     }
 
     @PostMapping("api/MetaDataList/delete/{projectId}")
-    public ResponseEntity<? extends  BasicResponse> deleteAllByMetaDataIdList(@PathVariable String projectId,@RequestBody List<String> metaDataIdList){
-        MetaDataDeleteAllRequestDto metaDataDeleteAllRequestDto = new MetaDataDeleteAllRequestDto(projectId,metaDataIdList);
+    public ResponseEntity<? extends BasicResponse> deleteAllByMetaDataIdList(@PathVariable String projectId,
+            @RequestBody List<String> metaDataIdList) {
+        MetaDataDeleteAllRequestDto metaDataDeleteAllRequestDto = new MetaDataDeleteAllRequestDto(projectId,
+                metaDataIdList);
         metaDataService.deleteAllByMetaDataIdList(metaDataDeleteAllRequestDto);
         return ResponseEntity.ok().body(new SuccessResponse());
     }
 
     // metatdata의 body부분을 수정
     @PutMapping("api/MetaData/{metadataId}")
-    public ResponseEntity<? extends  BasicResponse> update(@PathVariable String metadataId,@RequestBody Document body){
-        MetaDataUpdateRequestDto metaDataUpdateRequestDto = new MetaDataUpdateRequestDto(metadataId,body);
+    public ResponseEntity<? extends BasicResponse> update(@PathVariable String metadataId, @RequestBody Document body) {
+        MetaDataUpdateRequestDto metaDataUpdateRequestDto = new MetaDataUpdateRequestDto(metadataId, body);
         metaDataService.update(metaDataUpdateRequestDto);
         return ResponseEntity.ok().body(new SuccessResponse());
     }
 
     // metadataId를 기반으로 삭제
     @DeleteMapping("api/MetaData/{metadataId}")
-    public ResponseEntity<? extends  BasicResponse>  deleteById(@PathVariable String metadataId){
+    public ResponseEntity<? extends BasicResponse> deleteById(@PathVariable String metadataId) {
         metaDataService.deleteById(metadataId);
         return ResponseEntity.ok().body(new SuccessResponse());
     }
-
 
 }
